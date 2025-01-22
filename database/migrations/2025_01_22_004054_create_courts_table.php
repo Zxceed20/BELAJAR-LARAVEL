418210b640986976f4679f3950a9a00dd653c11f
Zxceed20 <zxceed430@gmail.com>
2025-01-22 08:40:15 +0700

    menambah database migration

--- database/migrations/2025_01_22_004054_create_courts_table.php
+++ database/migrations/2025_01_22_004054_create_courts_table.php
@@ -11,22 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('court_id')->constrained();
+            $table->text('name');
+            $table->integer('price');
             $table->timestamps();
         });
-    }
+    } 
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courts');
     }
 };
